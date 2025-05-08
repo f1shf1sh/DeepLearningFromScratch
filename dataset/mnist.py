386891d52a1318416f90db349d4b2f777bb6b52b
f1shf1sh <1284732495@qq.com>
2025-05-08 10:45:17 +0800

    add __init__.py file in dir. modify dataset/mnist.py, corrected file path issues

--- dataset/mnist.py
+++ dataset/mnist.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pickle
 import gzip
-from pathlib import Path
+import os
 
-pickle_path = "./"
-pickle_name = "dataset.pkl"
-data_dir = "./dataset/mnist_test_data"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MNIST_PATH = os.path.join(BASE_DIR, "mnist_test_data")
+DATASET_PKL_NAME = "dataset.pkl"
+CWD = os.getcwd()
 IMG_SIZE = 784
 
 file = {
@@ -16,8 +17,7 @@ file = {
 }
 
 def _load_img(file_name):
-    folder = Path(data_dir)
-    file_path = folder / file_name
+    file_path = os.path.join(MNIST_PATH, file_name)
     with gzip.open(file_path, "rb") as f:
         data = np.frombuffer(f.read(), np.uint8, offset=16)
     data = data.reshape(-1, IMG_SIZE)
@@ -25,7 +25,7 @@ def _load_img(file_name):
     return data 
 
 def _load_label(file_name):
-    file_path = data_dir + "/" + file_name
+    file_path = os.path.join(MNIST_PATH, file_name)
     with gzip.open(file_path, "rb") as f:
         data = np.frombuffer(f.read(), np.uint8, offset=8)
     
@@ -48,15 +48,25 @@ def _change_one_hot_label(X):
     return T
 
 def init_mnist():
-    file_path = pickle_path + pickle_name
+    file_path = os.path.join(CWD, DATASET_PKL_NAME)
     dataset = _convert_numpy()
     with open(file_path, "wb") as f:
         pickle.dump(dataset, f, -1) 
     
 def load_mnist(normlize = True, flatten = True, one_hot_label = False):
+    """_summary_
+
+    Args:
+        normlize (bool, optional): _description_. Defaults to True.
+        flatten (bool, optional): _description_. Defaults to True.
+        one_hot_label (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        _type_: _description_
+    """
     init_mnist()
     
-    file_path = pickle_path + pickle_name
+    file_path = os.path.join(CWD, DATASET_PKL_NAME)
     with open(file_path, "rb") as f:
         dataset = pickle.load(f)
 
