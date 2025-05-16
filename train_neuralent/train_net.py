ab5afc6495740e0d58987fd9d52862c422cd23a3
f1shf1sh <1284732495@qq.com>
2025-05-16 17:03:53 +0800

    feat(other): 从使用原始梯度下降法训练转换为误差反向传播，提高训练速度

--- train_neuralent/train_net.py
+++ train_neuralent/train_net.py
@@ -17,35 +17,38 @@ def timer(func):
 @timer
 def train_network():
     network = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
-    (train_img, train_label), (_, _) = load_mnist(normalize=True, flatten=True, one_hot_label=True)
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=True)
     
-    iters_num = 100
-    train_size = train_img.shape[0]
+    iters_num = 10000
+    train_size = x_train.shape[0] # 60000
     
     batch_size = 100
     learning_rate = 0.1
-    loss_train_list = [] 
+    loss_train_list = []
+    train_acc_list = []
+    test_acc_list = []
+    
+    iter_per_epoch = max(train_size / batch_size, 1) # 600
+    
     for i in range(iters_num): # get mini-batch
         batch_mask = np.random.choice(train_size, batch_size)
-        x_batch = train_img[batch_mask]
-        t_batch = train_label[batch_mask]
+        x_batch = x_train[batch_mask]
+        t_batch = t_train[batch_mask]
         
         # cal grads
-        grad = network.numerical_gradient(x_batch, t_batch)
+        grad = network.gradient(x_batch, t_batch)
         
         for key in ("W1", "b1", "W2", "b2"):
             network.params[key] -= learning_rate * grad[key] 
 
         loss = network.loss(x_batch, t_batch)
         loss_train_list.append(loss)
-        print(loss)
-        
-    plt.plot(loss_train_list)
-    plt.title("Loss List")
-    plt.xlabel("iters")
-    plt.ylabel("loss value")
-    plt.grid(True)
-    plt.savefig("iter_10000.png")
-
+        if i % iter_per_epoch:
+            train_acc = network.accuracy(x_train, t_train)
+            test_acc = network.accuracy(x_test, t_test)
+            train_acc_list.append(train_acc)
+            test_acc_list.append(test_acc)
+    print(train_acc_list, test_acc_list)
+  
 if __name__ == "__main__":
     train_network()
--- train_neuralent/two_layer_net.py
+++ train_neuralent/two_layer_net.py
@@ -3,6 +3,8 @@ sys.path.append(os.pardir)
 import numpy as np
 from common.gradient import numerical_gradient
 from common.functions import *
+from common.layers import *
+from collections import OrderedDict
 
 class TwoLayerNet:
     """
@@ -19,6 +21,12 @@ class TwoLayerNet:
         self.params["W2"] = wight_init_std * np.random.randn(hidden_size, output_size)
         self.params["b2"] = np.zeros(output_size)
         
+        # 生成层
+        self.layers = OrderedDict()
+        self.layers["Affine1"] = Affine(self.params["W1"], self.params["b1"])
+        self.layers["Affine2"] = Affine(self.params["W2"], self.params["b2"])
+        self.lastLayer = SoftMaxWithLoss() 
+         
     def predict(self, x):
         """
         Args:
@@ -26,16 +34,21 @@ class TwoLayerNet:
         Return:
              
         """
-        W1, W2 = self.params["W1"], self.params["W2"]
-        b1, b2 = self.params["b1"], self.params["b2"]
+        # version 1.0
+        # W1, W2 = self.params["W1"], self.params["W2"]
+        # b1, b2 = self.params["b1"], self.params["b2"]
 
-        a1 = np.dot(x, W1) + b1
-        z1 = sigmoid(a1)
+        # a1 = np.dot(x, W1) + b1
+        # z1 = sigmoid(a1)
         
-        a2 = np.dot(z1, W2) + b2
-        y = softmax(a2)
+        # a2 = np.dot(z1, W2) + b2
+        # y = softmax(a2)
         
-        return y
+        # version 2.0
+        for layer in self.layers.values():
+            x = layer.forward(x) 
+        return x
+    
     
     def loss(self, x, t):
         """
@@ -49,7 +62,7 @@ class TwoLayerNet:
         y = self.predict(x) 
         # y为100 x 10
         # x为100 x 10
-        return cross_entropy_error(y, t)
+        return self.lastLayer.forward(y, t)
     
     def accuracy(self, x, t):
         """ 准确率计算, accuracy = success_data / all_data
@@ -62,8 +75,9 @@ class TwoLayerNet:
         """
         y = self.predict(x)
         y = np.argmax(y, axis=1) # axis=1表示按照列进行方向进行压缩，列求和
-        t = np.argmax(t, axis=1)
-        
+        if t.ndim != 1:
+            t = np.argmax(t, axis=1)
+         
         accuracy = np.sum(y == t) / float(x.shape[0])
         
         return accuracy 
@@ -85,6 +99,29 @@ class TwoLayerNet:
         grads["b2"] = numerical_gradient(loss_W, self.params["b2"])
         
         return grads
+    
+    def gradient(self, x, t):
+        # forward
+        self.loss(x, t)
+        
+        # backward
+        dout = 1
+        dout = self.lastLayer.backward(dout)
+        
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+        
+        # 更新 
+        grads = {}
+        grads["W1"] = self.layers["Affine1"].dW
+        grads["b1"] = self.layers["Affine1"].db
+        grads["W2"] = self.layers["Affine2"].dW
+        grads["b2"] = self.layers["Affine2"].db
+        
+        return grads 
+        
         
 if __name__ == "__main__":
     net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
