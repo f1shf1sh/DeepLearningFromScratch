fix(softmax): 修复了softmax只能处理纬度为1的np.array, 添加纬度为2时的处理逻辑
原因:

无法处理纬度大于1的array，会导致输出nan而无法正常训练

状态:

完成,模型可以正常训练

common/functions.py
@@ -36,8 +36,14 @@ def softmax(x):
     Returns:
         _type_: _description_
     """
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T
+    
     c = np.max(x)
-    exp_a = np.exp(x -c)
+    exp_a = np.exp(x - c)
     sum_exp_a = np.sum(exp_a)
     
     return exp_a / sum_exp_a